98863c1451b200253d2d25276fd3444b6208f3e3
trueddd <kuzmichdima999@mail.ru>
2020-02-04 16:42:48 +0300

    implemented configuration gradle dsl module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,18 +14,18 @@ androidExtensions {
 }
 
 android {
-    buildToolsVersion = "29.0.1"
-    compileSdkVersion(29)
+    buildToolsVersion = Config.buildTools
+    compileSdkVersion(Config.VersionsSDK.compile)
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     defaultConfig {
-        applicationId = "com.github.trueddd.handy"
-        minSdkVersion(21)
-        targetSdkVersion(29)
-        versionCode = 1
-        versionName = "1.0"
+        applicationId = Config.appID
+        minSdkVersion(Config.VersionsSDK.min)
+        targetSdkVersion(Config.VersionsSDK.target)
+        versionCode = Config.versionCode
+        versionName = Config.versionName
     }
     lintOptions {
         isCheckReleaseBuilds = false
@@ -35,15 +35,12 @@ android {
 }
 
 dependencies {
-    val kotlinVersion = "1.3.61"
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
-    implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.core:core-ktx:1.1.0")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
-
-    implementation("com.github.trueddd:handy:0.0.1")
+    implementation(Dependencies.kotlinStdLib)
+    implementation(Dependencies.appCompat)
+    implementation(Dependencies.coreCompat)
+    implementation(Dependencies.constraintLayout)
+    implementation(Dependencies.Lifecycle.extensions)
+    implementation(Dependencies.Lifecycle.viewModel)
+    implementation(Dependencies.handy)
 }
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,28 @@
+object Dependencies {
+    private const val appCompatVersion = "1.1.0"
+    const val appCompat = "androidx.appcompat:appcompat:$appCompatVersion"
+
+    private const val coreVersion = "1.1.0"
+    const val coreCompat = "androidx.core:core:$coreVersion"
+
+    private const val constraintLayoutVersion = "1.1.3"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:$constraintLayoutVersion"
+
+    private const val lifecycleVersion = "2.2.0"
+    object Lifecycle {
+        const val extensions = "androidx.lifecycle:lifecycle-extensions:$lifecycleVersion"
+        const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion"
+    }
+
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Config.kotlinVersion}"
+
+    private const val gradleToolsVersion = "3.5.3"
+    const val gradleTools = "com.android.tools.build:gradle:$gradleToolsVersion"
+
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Config.kotlinVersion}"
+
+    private const val androidMavenGradlePluginVersion = "2.1"
+    const val androidMavenGradlePlugin = "com.github.dcendents:android-maven-gradle-plugin:$androidMavenGradlePluginVersion"
+
+    const val handy = "com.github.trueddd:handy:${Config.versionName}"
+}
